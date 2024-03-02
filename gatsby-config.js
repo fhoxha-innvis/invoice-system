46b69c2e97547ee314a89902f27f84bff104febd
franceskahoxha <franceskahoxha7@gmail.com>
2024-03-02 12:30:20 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteMetadata: {
+    title: `Invoice System`,
+    description: ``,
+    author: `@invoice-system.al`,
+    siteUrl: `localhost:8000`,
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Invoice System`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#FAFAFA`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // Adjust the path to your icon image
+      },
+    },
+  ],
+};
---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="border-b">
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
